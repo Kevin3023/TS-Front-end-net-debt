2bb8ded65a1dab9504c13f57cff52a7f2904ffa2
Kevin Gomes <kevin.gomes@ibm.com>
2022-05-05 15:44:46 -0300

    Navbar ok

--- src/Navbar/index.tsx
+++ src/Navbar/index.tsx
@@ -1,11 +1,17 @@
 import './styles.css';
+import Logo from './ibm-logo.png';
 
 function Navbar(){
     return (
         <nav className="main-navbar">
+            <div className='div-img'>
+            <img className='img' src={Logo} alt="Logo_IBM"/>
+            <a href='home' className='logo-text'>Desafio Public Sector Net Debt IBM</a> 
+            </div>
             
         </nav>
     )
 }
 
 export default Navbar;
+
